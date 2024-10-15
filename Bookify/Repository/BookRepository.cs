786d56d4189a8afa85ea2729f69cc8721883ec28
NetNinjaEngineer <mo5260287@outlook.com>
2024-10-15 11:09:20 +0300

    Ended Reviews

--- Bookify/Repository/BookRepository.cs
+++ Bookify/Repository/BookRepository.cs
@@ -7,34 +7,35 @@ namespace Bookify.Repository;
 
 public class BookRepository(ApplicationDbContext context) : GenericRepository<Book>(context), IBookRepository
 {
-    public async Task<IEnumerable<Book>> GetRelatedBooksAsync(int bookId)
-    {
-        var existedBook = await context.Books
-            .Include(x => x.Tags)
-            .Include(x => x.Author)
-            .Include(x => x.Publisher)
-            .FirstOrDefaultAsync(x => x.Id == bookId);
+	public async Task<IEnumerable<Book>> GetRelatedBooksAsync(int bookId)
+	{
+		var existedBook = await context.Books
+			.Include(x => x.Tags)
+			.Include(x => x.Author)
+			.Include(x => x.Publisher)
+			.FirstOrDefaultAsync(x => x.Id == bookId);
 
-        if (existedBook == null)
-            return [];
+		if (existedBook == null)
+			return [];
 
-        var bookTags = existedBook.Tags.Select(x => x.TagName).ToList();
+		var bookTags = existedBook.Tags.Select(x => x.TagName).ToList();
 
-        var relatedBooks = await context.Books
-            .Include(x => x.Tags)
-            .Include(x => x.Author)
-            .Include(x => x.Publisher)
-            .Where(x =>
-                x.Publisher!.PublisherName.Equals(existedBook.Publisher!.PublisherName, StringComparison.CurrentCultureIgnoreCase) ||
-                x.Author!.FullName.Equals(existedBook.Author!.FullName, StringComparison.CurrentCultureIgnoreCase) ||
-                x.Tags.Any(tag => bookTags.Contains(tag.TagName)) ||
-                x.EditionLanguage == existedBook.EditionLanguage)
-            .ToListAsync();
+		var relatedBooks = context.Books
+			.Include(x => x.Tags)
+			.Include(x => x.Author)
+			.Include(x => x.Publisher)
+			.Where(x =>
+				(
+					x.Author!.FirstName == existedBook.Author!.FirstName ||
+					x.Publisher!.PublisherName == existedBook.Publisher!.PublisherName ||
+					x.Tags.Any(tag => bookTags.Contains(tag.TagName))
+				) && x.Id != existedBook.Id)
+			.ToList();
 
-        if (relatedBooks.Count != 0)
-            return relatedBooks;
+		if (relatedBooks.Count != 0)
+			return relatedBooks;
 
-        return [];
+		return [];
 
-    }
+	}
 }
